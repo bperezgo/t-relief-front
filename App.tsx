1b53962e624e41cad5abb16ed36ed6038a0d701d
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-11 18:16:36 -0500

    feat: react native boilerplate

---
+++ App.tsx
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Text,
+  // useColorScheme,
+  View,
+  // https://github.com/transistorsoft/react-native-background-geolocation
+  // https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides-V5/emui-version-dependent-0000001050042515-V5
+} from 'react-native';
+
+const App = () => {
+  // const isDarkMode = useColorScheme() === 'dark';
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
+  return (
+    <View>
+      <Text>Hola T-Relief</Text>
+    </View>
+  );
+};
+
+export default App;
