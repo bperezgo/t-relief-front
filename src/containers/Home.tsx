32e7d09b055555c4a44e443d5dc0ccca503001ba
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-12 14:45:34 -0500

    feat: All views

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Text, Image, View} from 'react-native';
 import {Background} from '../components/Background';
+import {Colors, FontColors} from '../assets';
 
 export const Home = () => (
   <Background style={styles.contanier} colorSecondary="#fefefe">
     <Image style={styles.image} source={require('../assets/icons/logo.png')} />
-    <View>
-      {/* <Text>A trabajar</Text> */}
+    <View style={styles.callToAction}>
+      <Text style={styles.callToActionMessage}>A trabajar</Text>
     </View>
   </Background>
 );
@@ -22,5 +23,21 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     alignSelf: 'flex-start',
     marginLeft: 15,
+  },
+  callToAction: {
+    backgroundColor: Colors.lighter.mainBackWithOpacity,
+    width: 250,
+    height: 250,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    marginTop: 100,
+    borderRadius: 125,
+    elevation: 5,
+    opacity: 0.7,
+  },
+  callToActionMessage: {
+    color: FontColors.lighter.white,
+    fontSize: 36,
+    alignSelf: 'center',
   },
 });
